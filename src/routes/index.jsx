1ad4bca793d915a970adac47dc76f68049b17e2c
Mochammad nabiel <mochnabiel77@gmail.com>
2024-06-03 22:07:01 +0700

    resolve conflict at routes index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import HomePage from "@/pages/HomePage";
 import LoginPage from "@/pages/LoginPage";
 import OtpPage from "@/pages/OtpPage";
@@ -25,30 +24,3 @@ const router = createBrowserRouter([
 ])
 
 export default router
-=======
-import LoginPage from "@/pages/LoginPage";
-import OtpPage from "@/pages/OtpPage";
-import RegisterPage from "@/pages/RegisterPage";
-import { createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1 className="text-3xl font-bold">Home Page</h1>,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
-  {
-    path: "/otp",
-    element: <OtpPage />,
-  },
-]);
-
-export default router;
->>>>>>> 293af5ef5fed1e49ded1c9d665b5a0ced0292413
