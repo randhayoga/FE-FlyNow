a13b6ddf332257dbc6b885b3ae3c29afed410b06
Mochammad nabiel <mochnabiel77@gmail.com>
2024-06-03 22:12:37 +0700

    resolve other conflict

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-=======
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
->>>>>>> 293af5ef5fed1e49ded1c9d665b5a0ced0292413
 }
