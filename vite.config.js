d8ea7f5854094f2c1b51fe061ddf859dbbe21bc4
Mochammad nabiel <mochnabiel77@gmail.com>
2024-06-03 22:04:48 +0700

    resolve conflict at vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-=======
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
->>>>>>> 293af5ef5fed1e49ded1c9d665b5a0ced0292413
 
 export default defineConfig({
   plugins: [react()],
@@ -15,8 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> 293af5ef5fed1e49ded1c9d665b5a0ced0292413
