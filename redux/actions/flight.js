52cceb1afe718f4d052ebaf345cffcc3361897dc
stefhan235 <stefhansalim12@gmail.com>
2024-06-10 21:04:49 +0700

    add env

--- redux/actions/flight.js
+++ redux/actions/flight.js
@@ -3,7 +3,9 @@ import { setFlights } from "../reducers/flight";
 
 export const searchFlight = (queryParams) => async (dispatch) => {
   // Construct the dynamic API URL
-  let apiUrl = `http://localhost:3000/api/flight/search?da=${queryParams.da}&aa=${queryParams.aa}&dd=${queryParams.dd}&class=${queryParams.class}`;
+  let apiUrl = `${import.meta.env.VITE_BACKEND_API}/flight/search?da=${
+    queryParams.da
+  }&aa=${queryParams.aa}&dd=${queryParams.dd}&class=${queryParams.class}`;
 
   // Add optional parameters if they exist
   if (queryParams.rd) apiUrl += `&rd=${queryParams.rd}`;
