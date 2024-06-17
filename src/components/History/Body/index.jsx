2b64cc452099ecdaa8074dfb1e4d42606a0e7efc
Achmad Maulana Alawi <106646206+AchmadMaulanaAlawi@users.noreply.github.com>
2024-06-17 17:13:30 +0700

    History page (#24)
    * creating history page
    
    * Remove ADS from files
    
    * finishing history page
    
    * change primary color syntax and fixing url to get airline, departureAirport, and arrivalAirport
    
    * use env variable
    
    * integration to updated back-end api
    
    * revision: 1

---
+++ src/components/History/Body/index.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import NotFound from "./NotFound";
+import Cards from "./Cards";
+import Detail from "./Detail";
+
+function Body({ histories }) {
+  const [currentHistory, setCurrentHistory] = useState({});
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    if (histories.length > 0) {
+      setCurrentHistory(histories[0]);
+    }
+  }, [histories]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setModal(true);
+      } else {
+        setModal(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Set initial state based on window width
+    if (window.innerWidth >= 1024) {
+      setModal(true);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return histories.length === 0 ? (
+    <NotFound />
+  ) : (
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-8 mt-4 mb-16">
+      <Cards
+        histories={histories}
+        currentHistory={currentHistory}
+        setCurrentHistory={setCurrentHistory}
+        setModal={setModal}
+      />
+      {currentHistory?.id && (
+        <Detail
+          currentHistory={currentHistory}
+          modal={modal}
+          setModal={setModal}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/components/History/Header/Filter.jsx
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import { FaRegClock } from "react-icons/fa6";
+import { GiCancel } from "react-icons/gi";
+import { PiCityBold } from "react-icons/pi";
+import { CiFilter } from "react-icons/ci";
+import { FilterButton } from "@/components/ui/filterButton";
+import { useDispatch } from "react-redux";
+import {
+  searchHistoriesByCity,
+  searchHistoriesByPaymentStatus,
+} from "../../../../redux/actions/history";
+
+function Filter({ histories, setLoading, setFilter }) {
+  const dispatch = useDispatch();
+  const [cities, setCities] = useState([]);
+
+  const cityFiltering = async (city) => {
+    setFilter(
+      city
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+    );
+    setLoading(true);
+    await dispatch(searchHistoriesByCity(city));
+    setLoading(false);
+  };
+
+  const paymentStatusFiltering = async (paymentStatus) => {
+    setFilter(
+      paymentStatus
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+    );
+    setLoading(true);
+    await dispatch(searchHistoriesByPaymentStatus(paymentStatus));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    histories.map((history) => {
+      setCities((state) => [
+        ...state,
+        history.flight.departure.departureAirport.city.toLowerCase(),
+        history.flight.departure.arrivalAirport.city.toLowerCase(),
+      ]);
+      if (history.flight.return) {
+        setCities((state) => [
+          ...state,
+          history.flight.return.departureAirport.city.toLowerCase(),
+          history.flight.return.arrivalAirport.city.toLowerCase(),
+        ]);
+      }
+    });
+  }, [histories]);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <FilterButton variant="outline" className="flex gap-2">
+          <CiFilter className="text-lg" />
+          <div>Filter</div>
+        </FilterButton>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel className="font-semibold">
+          Penerbangan
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <PiCityBold className="me-2" />
+            <span>Kota</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              {cities.length > 0 &&
+                [...new Set(cities)].map((city) => {
+                  return (
+                    <DropdownMenuItem
+                      key={city}
+                      onClick={() => cityFiltering(city.toLowerCase())}
+                    >
+                      <span>
+                        {city
+                          .split(" ")
+                          .map(
+                            (word) =>
+                              word.charAt(0).toUpperCase() + word.slice(1)
+                          )
+                          .join(" ")}
+                      </span>
+                    </DropdownMenuItem>
+                  );
+                })}
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <FaRegCircleCheck className="me-2" />
+            <span>Status</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => paymentStatusFiltering("paid")}>
+                <FaRegCircleCheck className="me-2" />
+                <span>Selesai</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => paymentStatusFiltering("pending")}
+              >
+                <FaRegClock className="me-2" />
+                <span>Menunggu pembayaran</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => paymentStatusFiltering("unpaid")}
+              >
+                <GiCancel className="me-2" />
+                <span>Dibatalkan</span>
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default Filter;
