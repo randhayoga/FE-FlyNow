4299de29875092910979db55ee969099cb9d8504
PrakashFilemon <prakashfilemon@gmail.com>
2024-06-21 01:18:48 +0700

    Clean Code Profile

--- src/components/Profile/UbahProfile.jsx
+++ src/components/Profile/UbahProfile.jsx
@@ -1,8 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { editProfile, profile } from "../../../redux/actions/auth";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 
 const UbahProfile = () => {
@@ -13,8 +14,10 @@ const UbahProfile = () => {
 
   const [name, setName] = useState("");
   const [phoneNumber, setphoneNumber] = useState("");
+  const [isValidNoTelp, setIsValidNoTelp] = useState(false);
   const [image, setImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
+  const [isChanged, setIsChanged] = useState(false);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -23,6 +26,39 @@ const UbahProfile = () => {
       setImagePreview(URL.createObjectURL(file));
     }
   };
+
+  const noTelpChangeHandler = (e) => {
+    const value = e.target.value;
+
+    // Ignore if the value is not a number or the length is more than 15
+    if (isNaN(value.replace("+62 ", "")) || value.length > 16) {
+      return;
+    }
+
+    // This is so unncessary but cool as it will always keep +62 as the prefix and it cant be removed
+    if (value === "+62 ") {
+      setphoneNumber("");
+    } else if (value.startsWith("+62 ")) {
+      setphoneNumber(value);
+    } else if (value.length > 0) {
+      setphoneNumber("+62 " + value);
+    }
+
+    // Check if the phone number is valid
+    if (value.length >= 14 && value.length <= 16) {
+      setIsValidNoTelp(true);
+    } else {
+      setIsValidNoTelp(false);
+    }
+  };
+
+  useEffect(() => {
+    setIsChanged(
+      name !== user.name ||
+        phoneNumber !== user.phoneNumber ||
+        image !== user.image
+    );
+  }, [name, phoneNumber, image, user]);
 
   useEffect(() => {
     dispatch(profile());
@@ -50,7 +86,7 @@ const UbahProfile = () => {
       ) : (
         <div className="border w-auto">
           <div className="p-4 w-auto">
-            <form className="space-y-2" action="#" onSubmit={onSubmit}>
+            <form className="space-y-4" action="#" onSubmit={onSubmit}>
               <h1 className="text-lg font-bold">Ubah Data Profil</h1>
               <div className="border rounded-t-xl h-10 text-white bg-color-primary flex items-center">
                 <span className="m-auto text-lg ">Data Diri</span>
@@ -96,31 +132,40 @@ const UbahProfile = () => {
                     name="phoneNumber"
                     id="phoneNumber"
                     value={phoneNumber}
-                    onChange={(e) => setphoneNumber(e.target.value)}
+                    onChange={noTelpChangeHandler}
                     className="relative  border-slate-200 rounded-xl focus:ring-color-primary-500 focus:border-color-primary-500 block w-full p-2"
                     required
                   />
                 </div>
               </div>
-              <div className="relative border-2 border-slate-200 rounded-xl ">
-                <button className="file-upload text-sm py-3.5 px-4 text-color-primary bg-slate-50 rounded-l-lg text-uxl">
-                  Ubah Gambar Profil
-                </button>
 
-                <span>
+              <div className="relative border-2 border-slate-200 rounded-xl ">
+                <div className="flex items-center">
+                  <button className="file-upload text-sm py-3.5 px-4 text-color-primary bg-slate-50 rounded-l-lg text-uxl">
+                    Pilih Foto
+                  </button>
+                  <span
+                    id="file-chosen"
+                    className={`text-uxl ps-3 ${
+                      imagePreview ? "" : "text-slate-300"
+                    }`}
+                  >
+                    {imagePreview ? image.name : "Tidak ada file yang dipilih"}
+                  </span>
                   <Input
                     id="image"
                     type="file"
                     accept="image/*"
                     onChange={handleImageChange}
                     className="absolute inset-0 opacity-0 cursor-pointer"
-                    required
                   />
-                </span>
+                </div>
               </div>
+
               <button
                 type="submit"
-                className="w-32 h-10  text-white bg-color-primary  font-bold rounded-xl text-sm px-5 py-2.5 text-center"
+                className="w-32 h-10 mt-4 text-white bg-color-primary font-bold rounded-xl text-sm px-5 py-2.5 mx-auto block"
+                disabled={!isChanged}
               >
                 Simpan
               </button>
