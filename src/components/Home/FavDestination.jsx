abefaa468e5694f48789c27d65f8e999f9770e33
Mochammad nabiel <mochnabiel77@gmail.com>
2024-06-26 09:44:30 +0700

    add loading handler when get favorite flight from API & create loading skeleton for favorite destination card

--- src/components/Home/FavDestination.jsx
+++ src/components/Home/FavDestination.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { getFavoriteFlights } from "../../../redux/actions/flight";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -8,13 +9,18 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import { format } from "date-fns";
 import { id } from "date-fns/locale";
 
+import FavDestinationLoading from "./FavDestinationLoading";
+
 const FavDestination = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { favoriteFlights } = useSelector((state) => state.flights);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    dispatch(getFavoriteFlights());
+    dispatch(getFavoriteFlights(setIsLoading));
   }, [dispatch]);
 
   const formatDate = (date) => {
@@ -26,39 +32,50 @@ const FavDestination = () => {
     <div className="w-full mt-10">
       <h1 className="font-bold text-base mb-2">Destinasi Favorit</h1>
       <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-        {favoriteFlights.map((item) => (
-          <Card key={item.id} className="w-full rounded">
-            <CardHeader className="p-2 pb-0">
-              <div className="relative">
-                <div className="absolute top-0 right-0 flex items-center justify-end pe-2 rounded-s-xl w-2/5 h-6 bg-color-primary">
-                  <p className="font-normal text-[11px] text-white">
-                    {item.discount.toUpperCase()}
-                  </p>
+        {!isLoading ? (
+          favoriteFlights.map((item) => (
+            <Card
+              onClick={() => {
+                navigate(`/destinasi-favorit/${item.id}`);
+                window.scrollTo(0, 0);
+              }}
+              key={item.id}
+              className="w-full rounded cursor-pointer"
+            >
+              <CardHeader className="p-2 pb-0">
+                <div className="relative">
+                  <div className="absolute top-0 right-0 flex items-center justify-end pe-2 rounded-s-xl w-2/5 h-6 bg-color-primary">
+                    <p className="font-normal text-[11px] text-white">
+                      {item.discount.toUpperCase()}
+                    </p>
+                  </div>
+                  <img
+                    src={item.image}
+                    alt={`Destination image ${item.id}`}
+                    className="rounded w-full sm:h-36 md:h-32 lg:h-28 object-cover"
+                  />
                 </div>
-                <img
-                  src={item.image}
-                  alt={`Destination image ${item.id}`}
-                  className="rounded w-full sm:h-32 md:h-28 lg:h-24 object-cover"
-                />
-              </div>
-              <CardTitle className="flex items-center gap-2 font-medium text-sm">
-                {item.departureCity}
-                <FaArrowRightLong />
-                {item.arrivalCity}
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="text-xs leading-5 px-2">
-              <p className="font-bold text-color-primary">{item.airline}</p>
-              <p className="font-medium">{formatDate(item.departureTime)}</p>
-              <p className="text-sm font-normal">
-                Mulai dari{" "}
-                <span className="font-bold text-alert-danger">
-                  IDR {item.price.toLocaleString("id-ID")}
-                </span>
-              </p>
-            </CardContent>
-          </Card>
-        ))}
+                <CardTitle className="flex items-center gap-2 font-medium text-sm">
+                  {item.departureCity}
+                  <FaArrowRightLong />
+                  {item.arrivalCity}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="text-xs leading-5 px-2">
+                <p className="font-bold text-color-primary">{item.airline}</p>
+                <p className="font-medium">{formatDate(item.departureTime)}</p>
+                <p className="text-sm font-normal">
+                  Mulai dari{" "}
+                  <span className="font-bold text-alert-danger">
+                    IDR {item.price.toLocaleString("id-ID")}
+                  </span>
+                </p>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <FavDestinationLoading />
+        )}
       </div>
     </div>
   );
